2c26de0f273c44d7ae5830db2000e52cf0a0afd9
kevin Oliveira <kevinklgvg@gmail.com>
2022-02-08 22:07:49 -0300

    add handleSubmit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,93 @@
+// Types
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 
-import styles from "../styles/Home.module.css";
+//Styles
+import styles from "../styles/Home.module.css"; // todos os estilos...
+
+//import system
 import { useForm } from 'react-hook-form';
 
-//Exemplo
+//types page
 type SingIn = {
   email:string
   password:string
 }
 
+// OBS :: implementar graphQl para trazer apenas os dados necessários para cada pagina...
+
 const Home: NextPage = ( props: InferGetStaticPropsType<typeof getStaticProps>) => {
 
   const {register, handleSubmit} = useForm<SingIn>();
 
+  // Verificar o TOKEN_ID nos cookies se está valido....
+  // caso não esteja SingIn ? /dashboard ... 
+
   const singIn = async (data:SingIn) => {
-    console.log(data);
+    //pega os dados de acesso e verifica se está cadastrado...
+    console.log(data, props);
   }
 
 
-  return (
-    <div className={styles.container}>
+  if(false){ // verifica se a Token ID (alterar para o token)
+    // nunca entra nesse if como false...
+    return(
 
-      <form onSubmit= { handleSubmit(singIn) }>
+      <div>
 
-        <input 
-          { ...register('email') }
-          type="email" 
-          name="email" 
-          placeholder="E-mail:"
-        />
+        <h1>Sorry! Not alowedd</h1>
 
-        <br/>
+      </div>
 
-        <input
-          { ...register('password') }
-          type='password'
-          name='password'
-          placeholder="Password:"
-        />
+    );
+  } else {
 
-        <br/>
+    return (
+      <div className={styles.container}>
+
+        <form onSubmit= { handleSubmit(singIn) }>
         
-        <button type="submit"> Sing In </button>
+          {/* Pegar dois campos (email e senha) e enviar*/}
 
-      </form>
+          <input 
+            { ...register('email') }
+            type="email" 
+            name="email" 
+            placeholder="E-mail:"
+            //campo de email
+          />
 
-    </div>
-  );
-};
+          <br/>
 
+          <input
+            { ...register('password') }
+            type='password'
+            name='password'
+            placeholder="Password:"
+            //campo password
+          />
+
+          <br/>
+
+          <button 
+            type="submit"
+            // button submit 
+          > 
+            Sing In 
+          </button>
+
+        </form>
+
+      </div>
+    );
+  };
+}
 export const getStaticProps: GetStaticProps = async () => {
+
+  //pegar todos os usuarios... (fetch api)
 
   return {
     props: {
-      
+      //passar os usuarios
     },
   };
 };
