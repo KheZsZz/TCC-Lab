0cdfcc2c9613973378dae53412dacdfafc982cbd
Kevin Oliveira <kevinklgvg@gmail.com>
2022-04-13 15:57:01 -0300

    add shortcuts path

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,20 @@
 export const manisfest = {
-    base_url:"http://localhost:3000/api/"
+    base_url:"http://localhost:3000/api/",
+    tablesBD:{
+        
+        Users:{
+            users:"Users_tbl",
+            employee:"Employee_tbl",
+            students:"Students_tbl",
+        },
+
+        Institution:{
+            adress:"Adress_tbl",
+            institution:"Instruction_tbl",
+            educationHub:"EducationHub_tbl",
+            laboratory:"labs_tbl",
+            courses:"Courses_tbl",
+            schoolSubject:"SchoolSubject_tbl"
+        }
+    }
 }
