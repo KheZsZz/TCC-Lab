4bc85acfb7c19080c6057233a375d0af6801b5bf
Kevin Oliveira <kevinklgvg@gmail.com>
2022-04-20 17:02:40 -0300

    alter table users_tbl

---
+++ src/utils/insert user.sql
@@ -0,0 +1,11 @@
+use tcc;
+
+insert into tcc.adress_tbl values
+(null, "Rua", "Agnaldo Ferreira Lopes", 5, "Casa 01", "Potuverá", "Itapecerica da Serra", "SP", "06852845"),
+(null, 'Estrada', 'Emiliana Emery', 494, 'Portão 02', 'Potuverá', 'Itapecerica da Serra', 'SP', '06852870');
+
+insert into instruction_tbl values
+(null, 2, 'Ecobus Transportes e Turismo Ltda Epp', '19.463.849/0001-20', '(11) 9.9577-8573', 'kevin@ecobusturismo.com.br', 'Geraldo Mendes Soares');
+
+insert into users_tbl values
+(null, 1, 1, 'kevin', 'Oliveira', '238.610.668-31', '53.806.243-5', '(11) 9.99577-8573', 'kevinklgvg@gmail.com', 'Asevenfold12@');
