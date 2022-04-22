cc77ec0e3716758be04f4085ea82e7bb28604e49
Kevin Oliveira <kevinklgvg@gmail.com>
2022-04-22 09:17:10 -0300

    initial route adress

---
+++ src/pages/api/adress/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { manisfest } from "../../../config/config";
+import { sql_query } from "../../../config/database";
+
+
+const handleAdress = async (req:NextApiRequest, res:NextApiResponse) => {
+  switch(req.method){
+    case "GET":
+      const data = await sql_query(`SELECT * FROM ${manisfest.tablesBD.Institution.adress}`,[]);
+      res.status(200).json(data);
+    break;
+
+    default:
+      res.status(404).json({message:`Sorry! Bad request... Error 404`})
+  }
+}
+export default handleAdress;
