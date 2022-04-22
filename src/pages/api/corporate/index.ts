f01fb4a1ba430483cb3611d8f2da1b1ba7b10931
Kevin Oliveira <kevinklgvg@gmail.com>
2022-04-22 09:27:18 -0300

    alter sql query

--- src/pages/api/corporate/index.ts
+++ src/pages/api/corporate/index.ts
@@ -10,13 +10,22 @@ const handleCorporate = async (res:NextApiResponse, req:NextApiRequest) => {
 
     case "POST":
       const corporate:Institution = req.body;
-      await sql_query<Institution>(
-        `INSERT INTO ${manisfest.tablesBD.Institution.institution} VALUES ()`,
-      []);
+      await sql_query<Institution> (
+        `INSERT INTO ${manisfest.tablesBD.Institution.institution}
+        VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [
+        null, 
+        corporate.address.id, 
+        corporate.corporate_name, 
+        corporate.cnpj, 
+        corporate.phone,
+        corporate.email,
+        corporate.responsible
+      ]);
     break;
 
     default:
-      res.status(200).json({ message:'Sorry! Bad request error 404' });
+      res.status(400).json({ message:'Sorry! Bad request error 404' });
   }
 }
 export default handleCorporate;
