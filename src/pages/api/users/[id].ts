4b844029c5a61f405d889aa9a98cc74c3087afa0
Kevin Oliveira <kevinklgvg@gmail.com>
2022-04-25 16:54:36 -0300

    add route "delete" users

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,23 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { manisfest } from "../../../config/config";
 import { sql_query } from "../../../config/database";
-
+import type { Users } from '../../../types/type_users';
 
 const handleUserID = async (req:NextApiRequest, res:NextApiResponse) => {
   const { id } = req.query;
+  const user:Users = req.body;
 
   switch(req.method){
     case "GET":
-      const data = await sql_query(`SELECT * FROM ${manisfest.tablesBD.Users} WHERE id = ?`, [Number(id)]);
+      const data = await sql_query<Users>(`select * from ${manisfest.tablesBD.Users.users} where id = ?`, [Number(id)]);
       res.status(200).json(data)
     break;
 
-    case "PATH":
-
+    case "PATCH":
+      
     break;
 
     case "DELETE":
-      await sql_query(``,[id, 0])
+      try {
+        const verify:Users[] = await (await fetch(`${manisfest.base_url}users/${id}`)).json();
+        if(verify.length){
+          await sql_query(`update ${manisfest.tablesBD.Users.users} set verify = ? where id = ?`,[0, Number(id)]);
+          res.status(200).json({message:`Usuário ${id} deletado com sucesso!`})
+        }else {
+          res.status(204).json({message:`Usuário ${id} não encontrado!`});
+        }
+      } catch (error) {
+        res.status(404).json({message:`Error ao deletar o usuário ${id}!`});
+      }
     break;
 
     default:
