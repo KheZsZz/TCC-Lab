bdcc60272fe0e0b83b6b2d9b7773f3e317eebc4c
Kevin Oliveira <kevinklgvg@gmail.com>
2022-05-05 16:13:09 -0300

    .ev.example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import { NextPage } from 'next';
+import {
+  GetServerSideProps,
+  NextPage,
+  InferGetServerSidePropsType,
+} from 'next';
+import Router from 'next/router';
+import cookie from 'nookies';
+import { decode } from 'jsonwebtoken';
 
-const Home: NextPage = () => {
+const Home: NextPage = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>,
+) => {
   return (
     <>
       <h1>Dashboard</h1>
     </>
   );
 };
-
+const getServerSideProps: GetServerSideProps = async () => {
+  const token = cookie.get();
+  !token ? Router.push('/login') : null;
+  return {
+    props: {},
+    redirect: {
+      statusCode: 401,
+      basePath: '/login',
+    },
+  };
+};
 export default Home;
